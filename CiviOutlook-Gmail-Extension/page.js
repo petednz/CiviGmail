a16f62926f2f16d8506c82e30fa13ec8c2e036f5
Gopi Krishna <gopi@vedaconsulting.co.uk>
2016-08-31 18:32:44 +0100

    Connect to outlook api and log activities on email sent & from inbox

--- CiviOutlook-Gmail-Extension/page.js
+++ CiviOutlook-Gmail-Extension/page.js
@@ -1,3 +1,6 @@
+var gmail;
+var serverUrl = 'https://mailchimp.vedaconsulting.co.uk/civicrm/gmail/logactivity';
+
 function refresh(f) {
   if((/in/.test(document.readyState)) || (undefined === window.Gmail) || (undefined === window.jQuery)) {
     console.log('waiting for 10 sec');
@@ -9,14 +12,265 @@ function refresh(f) {
 }
 
 var main = function() {
-  var gmail = new Gmail();
+  gmail = new Gmail();
   bttn = gmail.tools.add_toolbar_button('Connect Outlook' , reConnect);
   bttn.addClass('coge_bttn_container');
 
   // add test button on Gmail tool bar TEST
-  gmail.tools.add_toolbar_button('Fetch Civi Data' , makeCorsRequest);
+  // gmail.tools.add_toolbar_button('Fetch Civi Data' , makeCorsRequest);
+
+  $userEmail = gmail.get.user_email();
+  console.log("user email : ", $userEmail);
+
+  // Add button to record received email data
+  gmail.tools.add_toolbar_button('Record Activity' , recordActivityFromInbox);
+
+  // on Email sent
+  gmail.observe.after("send_message", recordActivityOnEmailsent);
+
+   // add test button on Gmail tool bar TEST
+  // gmail.tools.add_toolbar_button('Email Sent' , testfunc);
+
 }
 refresh(main);
+
+// Function to record activity for selcted email
+function recordActivityFromInbox(){
+  // get slected emails data
+  var selectedEmailsData = gmail.get.selected_emails_data();
+
+  if (selectedEmailsData.length > 0) {
+    for (var i = 0; i < selectedEmailsData.length; i++) {
+      console.log('selectedEmailsData[i]', selectedEmailsData[i]);
+      // get the last email id from the thread
+      var latestEmailId = selectedEmailsData[i].last_email;
+
+      // get the email data of the last email from the thread
+      for(var key in selectedEmailsData[i].threads){
+        if (key == latestEmailId) {
+          var emailData = selectedEmailsData[i].threads[key];
+        }
+      }
+
+      // get required values to pass to civi
+      var email = emailData.from_email;
+      var emailSubject = emailData.subject;
+      var emailBody = emailData.content_plain;
+      // get attachments, if any
+      var emailAttachments = [];
+      if (emailData.attachments.length > 0) {
+
+        for (var key in emailData.attachments) {
+          // attached file name
+          var fileName = emailData.attachments[key];
+
+          // get file source URL
+          // https://mail.google.com/mail/u/0/?ui=2&ik={ik_value}&view=att&th={message_id}&attid=0.{atachment_index}&disp=safe&zw
+          var ik = gmail.tracker.ik;
+          var id = latestEmailId;
+          var aid = key+1; // gets the first attachment
+          var fileUrl = "https://mail.google.com/mail/u/0/?ui=2&ik=" + ik + "&view=att&th=" + latestEmailId + "&attid=0." + aid + "&disp=safe&zw";
+
+          // attached file object
+          var attachedFile = {name:fileName, url:fileUrl};
+
+          // push attachedFile into emailAttachment array
+          emailAttachments.push(attachedFile);
+
+        }
+
+      }
+      // call log activity API
+      callLogActivityAPI(email, emailSubject, emailBody, emailAttachments);
+    }
+  } else{
+    // if no emails selected, instruct to select one
+    console.log('no emails selected');
+    alert('select atleast one email to continue');
+  }
+}
+
+// Function to collect details and pass on Email sent
+function recordActivityOnEmailsent(url, body, data, response, xhr){
+  console.log('data', data);
+  // array of recipient email addresses
+  var emailAddresses = [];
+  emailAddresses = data.to;
+
+  // get email details
+  var emailSubject = data.subject;
+  // FIX ME : email body appears with html tags
+  var emailBody = data.body;
+  var emailAttachments = [];
+
+  for (var i = 0; i < emailAddresses.length; i++) {
+    // email sent action returns an empty value in To emails array
+    // therefore, proceeding only if it is not empty
+    if (emailAddresses[i] != '') {
+      // call log activity api in civi
+      callLogActivityAPI(emailAddresses[i], emailSubject, emailBody, emailAttachments);
+    }
+  }
+}
+
+// Function to make initial API request with the email address
+function callLogActivityAPI(email, emailSubject, emailBody, emailAttachments){
+
+  // if empty email address
+  if (email == '') {
+    console.log('email address not found');
+    return;
+  }
+
+  // JSON stringify the emailAttachemnt array
+  var emailAttachment = emailAttachments;
+
+  // assign initial data from sent email into params
+  var params = {email: email, subject: emailSubject, email_body: emailBody, email_attachment: emailAttachment};
+  console.log('params in call log activity api', params);
+
+
+  // API call with intial sent email data
+  makeHttpRequest('GET', params);
+
+}
+
+// Function to make HTTP request
+function makeHttpRequest(method, params){
+  console.log('making api call . . .');
+
+  $.ajax({
+      method:method,
+      url: serverUrl,
+      data: params,
+      dataType: "text",
+      crossDomain: true,
+      success: function (data, textStatus ) {
+        // return Parsed data
+        result = JSON.parse(data);
+        callActivityConfirmation(result, params);
+      },
+      error: function(xhr, textStatus, errorThrown){
+        console.log('error test', xhr);
+        return;
+      }
+  });
+
+}
+
+// Process HTTP response and call relevant confirmation screens
+function callActivityConfirmation(result, params){
+
+  // Display error message and exit, if any
+  if (result.is_error) {
+    displayErrorMessage(result.error_message);
+    return;
+  }
+
+  // if single contact exists in Civi
+  if (result.singleContactExists) {
+    // convert contact Id into integer and assign into api param as ot_target_contact_id
+    params['ot_target_contact_id'] = parseInt(result.singleContactExists);
+    console.log('single contact found in civi : ', result);
+    // display Activity Confirmation screen
+    activityConfirmationScreen(params);
+  }
+
+  // if single contact created in Civi
+  if (result.singleContactCreated) {
+    // convert contact Id into integer and assign into api param as ot_target_contact_id
+    params['ot_target_contact_id'] = parseInt(result.singleContactCreated);
+    console.log('single contact created in civi : ', result);
+    // display Activity Confirmation screen
+    activityConfirmationScreen(params);
+  }
+
+  // if multiple contacts exists in Civi
+  if (result.count && result.count > 1) {
+
+    var duplicateContacts = result.values;
+    console.log('duplicate contacts found in civi : ', duplicateContacts);
+    // select form HTML
+    var selectForm = '<div>Select one contact from the list below</div>';
+    for (var key in duplicateContacts) {
+        // add each contact as option to select
+        selectForm = selectForm + '<div><input type="radio" name="contact_id" value='+key+'> '
+        +duplicateContacts[key].sort_name
+        +'</div>';
+    }
+    selectForm = selectForm + '</form>';
+    // END of select form HTML
+
+    // display Duplicate contacts with option to select
+    displayDuplicateContacts(selectForm, params);
+  }
+
+  // if id found in result, activity has been created
+  if (result.id) {
+    // TODO : Display message
+  }
+
+}
+
+// Function to display Duplicate Contacts
+function displayDuplicateContacts(selectForm, params){
+  console.log('displaying duplicate contacts . . .');
+   gmail.tools.add_modal_window('Duplicate Contacts found', selectForm, function(){
+    params['ot_target_contact_id'] = $('input[name="contact_id"]:checked').val();
+    // display Activity Confirmation screen
+    activityConfirmationScreen(params);
+    // remove model window on Click OK
+    gmail.tools.remove_modal_window();
+  });
+}
+
+
+// Function to Display Activity Confirmation screen
+function activityConfirmationScreen(params){
+  console.log('displaying confirmation screen ', params);
+  var formCSS = '<style>'
+  +'.gopi{color:red;}'
+  +'div{margin-top:5px;}'
+  +'</style>'
+  ;
+
+  var confirmationForm = '<form>'
+  +'<div>Subject:</div>'
+  +'<textarea name="subject" rows="1" cols="50">'
+  + params.subject
+  +'</textarea>'
+  +'<div>Details:</div>'
+  +'<textarea name="email_body" rows="10" cols="50">'
+  + params.email_body
+  +'</textarea>'
+  +'<br><br>'
+  +'</form>'
+  ;
+
+  var formHtml = formCSS+confirmationForm;
+
+  gmail.tools.add_modal_window('Create Activity', formHtml, function(){
+    // assign form submitted values to params
+    params['subject'] = $('textarea[name="subject"]').val();
+    params['email_body'] = $('textarea[name="email_body"]').val();
+    console.log('creating activity', params);
+    // create activity
+    makeHttpRequest('POST', params);
+    // remove model window on Click OK
+    gmail.tools.remove_modal_window();
+  });
+}
+
+// Function to display Error message
+function displayErrorMessage(errorMessage){
+  var errorMessage = '<div>'+ errorMessage +'</div>';
+  gmail.tools.add_modal_window('Error', errorMessage, function(){
+    // remove model window on Click OK
+    gmail.tools.remove_modal_window();
+  });
+}
+
+
 
 function reConnect() {
   //detail could be used to pass more info
@@ -24,7 +278,7 @@ function reConnect() {
 }
 
 // Create the XHR object.
-function createCORSRequest(method, url) {
+/*function createCORSRequest(method, url) {
   var xhr = new XMLHttpRequest();
   if ("withCredentials" in xhr) {
     // XHR for Chrome/Firefox/Opera/Safari.
@@ -43,7 +297,7 @@ function createCORSRequest(method, url) {
 // Make the actual CORS request.
 function makeCorsRequest() {
   // All HTML5 Rocks properties support CORS.
-  var url = 'https://cms46.loc/civicrm/gmail/logactivity';
+  var url = 'https://mailchimp.vedaconsulting.co.uk/civicrm/gmail/logactivity';
 
   var xhr = createCORSRequest('GET', url);
   if (!xhr) {
@@ -63,4 +317,28 @@ function makeCorsRequest() {
   };
 
   xhr.send();
+}*/
+
+
+// TEST Function to use instead of email sent
+function testfunc(){
+
+  //  hardcoded for testing
+  // var emailAddresses = ["Gopi Krishna <gopigmail1@vedaconsulting.co.uk>", "gopigmail2@vedaconsulting.co.uk", ""];
+  var emailAddresses = ["Gopi Krishna <gopigmail4@vedaconsulting.co.uk>", ""];
+
+  var emailSubject = 'new contact ';
+  var emailBody = 'sample email body for new contact';
+  var emailAttachments = [];
+
+
+  for (var i = 0; i < emailAddresses.length; i++) {
+    console.log('single email address ', emailAddresses[i]);
+    // email sent action returns empty values in To emails array
+    if (emailAddresses[i] != '') {
+      // call log activity api in civi
+      callLogActivityAPI(emailAddresses[i], emailSubject, emailBody, emailAttachments);
+    }
+  }
+
 }
--- uk.co.vedaconsulting.gmailapi/CRM/Gmailapi/Page/AJAX.php
+++ uk.co.vedaconsulting.gmailapi/CRM/Gmailapi/Page/AJAX.php
@@ -1,142 +1,271 @@
 <?php
 
 class CRM_Gmailapi_Page_AJAX {
-  static function authenticate() {
-    $output = array();
-    CRM_Core_Error::debug_var('$_REQUEST', $_REQUEST);
 
-    // $result = self::authorizationCodeRequest('token');
-    // //$this->assertEqual($result->code, 302, 'The implicit flow request completed successfully');
-    // $redirect_url_parts = explode('#', $result->redirect_url);
-    // $response = drupal_get_query_array($redirect_url_parts[1]);
-    // //$this->assertTokenResponse($response, FALSE);
+  static function authenticate(){
 
-    $verification_url = url('oauth2/tokens/' . $_REQUEST['oauth_token'], array('absolute' => TRUE));//, 'query' => array('scope' => 'gmail_extension')));
-    CRM_Core_Error::debug_var('$verification_url', $verification_url);
-    $result = self::httpRequest($verification_url);
-    CRM_Core_Error::debug_var('$result', $result);
-    $verification_response = json_decode($result->data);
-    CRM_Core_Error::debug_var('$verification_response', $verification_response);
+    $result = array();
+    $email = '';
+    $processAttachment= FALSE;
 
-    if ($result->code == '200' && !empty($verification_response->user_id) && ($verification_response->access_token == $_REQUEST['oauth_token'])) {
-      // consider logged IN
-      $output[] = 'logged IN';
+    // function to validate token
+    // $isValidUser = validateUser();
+    // FIXME ::: setting validuser to true & assign source contact for now
+    $isValidUser = TRUE;
+    // FIX ME: get source contacts api key and assign it into the request
+    // getApiKey();
+    $_REQUEST['api_key'] = 'server123';
 
-      // DS: to programmatically force user login. It also initializes civi
-      //$form_state = array();
-      //$form_state['uid'] = $verification_response->user_id;
-      //CRM_Core_Error::debug_var('$form_state', $form_state);
-      //user_login_submit(array(), $form_state);
-    } else {
-      $output[] = 'logged OUT';
+    if ($isValidUser) {
+
+      $params = array();
+      // if email address found, assign params to call activty API
+      if (isset($_REQUEST['email'])) {
+        // extract valid email address from the email string : Eg: Gopi Krishna <gopi@vedaconsulting.co.uk>
+        $email = self::extractValidEmail($_REQUEST['email'], '<', '>');
+        $params['email'] = $email;
+      }
+      if (isset($_REQUEST['subject'])) {
+        $params['subject'] = $_REQUEST['subject'];
+      }
+      if (isset($_REQUEST['email_body'])) {
+        $params['email_body'] = $_REQUEST['email_body'];
+      }
+      if (isset($_REQUEST['ot_target_contact_id'])) {
+        $params['ot_target_contact_id'] = $_REQUEST['ot_target_contact_id'];
+      }
+      // if email attachment not empty, set process attchment true
+      if (isset($_REQUEST['email_attachment']) && !empty($_REQUEST['email_attachment'])) {
+        $processAttachment = TRUE;
+        $params['attachment'] = $_REQUEST['email_attachment'];
+      }
+
+      // FIX ME : using email activity type id for now
+      $params['activity_type_id'] = 3;
+
+    } else{
+      $result['is_error'] = 1;
+      $result['message'] = 'Please authenticate your Gmail with Civi';
+      // return API result with error message, if not a valid user
+      self::returnApiResult($result);
     }
 
-    /*
-    $loggedIn = FALSE;
+    // if valid email address found, continue with log activity API call
+    if (!empty($email) && !empty($params) ) {
+      CRM_Core_Error::debug_var('API $params', $params);
+      // call createactivity API in outlook API extension
+      $result = civicrm_api3('CiviOutlook', 'createactivity', $params);
 
-    //check session to check the login status
-   /* $session =& CRM_Core_Session::singleton();
-    $userID = $session->get('ufID');
+      // Process Attachment, if activity created and attachments found in the request
+      if ($result['id'] && $processAttachment){
+        $attachments = $params['attachment'];
+        if(!empty($attachments)){
 
-    // ask user to log in to Civi, if they haven't logged in yet
-    if (isset($userID) || !empty($userID)) {
-      $loggedIn = TRUE;
-    }else {
-      $response['is_loggedin'] = $loggedIn;
-      $response['message'] = 'Please Login to your Civi in another tab to continue!';
-      CRM_Utils_JSON::output($response);
-      CRM_Utils_System::civiExit();
-    }*/
+          // TO DO : Process attachments
+          foreach ($attachments as $key => $value) {
+            # code...
+          }
+          CRM_Core_Error::debug_var('$attachment in final act result ', $attachments);
+        }
+      }
 
-    // test
+      // return API response received from outlook API
+      self::returnApiResult($result);
 
-    /*
-     $loggedIn = TRUE;
-
-
-    if (isset($_GET['email']) && !empty($_GET['email']) && $loggedIn) {
-      $email = $_GET['email'];
-
-      $response = _civicrm_api3_civi_gmailapi_civicrm_api_wrapper('Contact', 'get', array('email' => $email));
-    } else {
-      $loggedIn = FALSE;
-      $response['message'] = 'Some technical error. Please contact admin!';
+    } else{
+      $result['is_error'] = 1;
+      $result['message'] = 'No Valid email address found to record activity';
+      // return API result with error message, if no valid email found
+      self::returnApiResult($result);
     }
-    $response['is_loggedin'] = $loggedIn;
-     */
-    CRM_Core_Error::debug_var('$_SERVER', $_SERVER);
-    // FOR GMAIL CORS request - Request header field Access-Control-Allow-Origin is not allowed by Access-Control-Allow-Headers in preflight response.
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-      header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-      header('Access-Control-Allow-Credentials: true');
-      header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
 
-      if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    }
-    CRM_Utils_JSON::output($output);
+  }
+
+  static function returnApiResult($result){
+    CRM_Core_Error::debug_var('API $result', $result);
+    CRM_Utils_JSON::output($result);
     CRM_Utils_System::civiExit();
   }
 
-  /**
-   * Performs an authorization request and returns it.
-   *
-   * Used to test authorization, the implicit flow, and the authorization_code
-   * grant type.
-   *
-   * @return
-   *   The return value of $this->httpRequest().
-   */
-  static function authorizationCodeRequest($response_type, $scope = NULL) {
-    $clientId = 'client123id';
-    $query = array(
-      'response_type' => $response_type,//code, token
-      'client_id' => $clientId,
-      'state' => drupal_get_token($clientId),
-      // The "authorized" url doesn't actually exist, but we don't need it.
-      'redirect_uri' => url('authorized', array('absolute' => TRUE)),
-      // OpenID Connect requests require a nonce. Others ignore it.
-      'nonce' => 'test',
-      'scope' => 'gmail_extension',
-    );
+  // Function to to extract valid email addres from email string
+  static function extractValidEmail($string, $start, $end){
+    $pos = stripos($string, $start);
+    // return the actual email string, if '<' not found in the email string
+    if ($pos == '') {
+      return $string;
+    }
 
-    $authorize_url = url('oauth2/authorize', array('absolute' => TRUE, 'query' => $query));
-    return self::httpRequest($authorize_url);
+    $str = substr($string, $pos);
+
+    $str_two = substr($str, strlen($start));
+
+    $second_pos = stripos($str_two, $end);
+
+    $str_three = substr($str_two, 0, $second_pos);
+
+    $unit = trim($str_three); // remove whitespaces
+
+    return $unit;
   }
 
-  /**
-   * Performs a drupal_http_request() with additional parameters.
-   *
-   * Passes along all cookies. This ensures that the test user has access
-   * to the oauth2 endpoints.
-   *
-   * @param $url
-   *   $url: A string containing a fully qualified URI.
-   * @param $options
-   *   The options array passed along to drupal_http_request().
-   *
-   * @return
-   *   The result object as returned by drupal_http_request().
-   */
-  static function httpRequest($url, array $options = array()) {
-    // Forward cookies.
-    $cookie_string = '';
 
-    //foreach ($this->cookies as $name => $data) {
-    //  $cookie_string .= $name . '=' . $data['value'] . ';';
-    //}
+  // static function authenticate() {
 
-    $options['headers']['Cookie'] = $cookie_string;
+  //   CRM_Core_Error::debug_var('$_REQUEST', $_REQUEST);
 
-    // Set other general options.
-    $options += array(
-      'max_redirects' => 0,
-    );
+  //   $params = array();
+  //   foreach ($_GET as $key => $value) {
+  //     $params[$key] = $value;
+  //   }
 
-    return drupal_http_request($url, $options);
-  }
+  //   CRM_Core_Error::debug_var('$params', $params);
+
+  //   $contactResult = civicrm_api3('Contact', 'get', array(
+  //     'sequential' => 1,
+  //     'email' => "workatveda@gmail.com",
+  //   ));
+
+  //   CRM_Core_Error::debug_var('contact $result', $contactResult);
+
+  //   return $contactResult;
+
+  //   exit();
+
+  //   $output = array();
+  //   CRM_Core_Error::debug_var('$_REQUEST', $_REQUEST);
+
+  //   // $result = self::authorizationCodeRequest('token');
+  //   // //$this->assertEqual($result->code, 302, 'The implicit flow request completed successfully');
+  //   // $redirect_url_parts = explode('#', $result->redirect_url);
+  //   // $response = drupal_get_query_array($redirect_url_parts[1]);
+  //   // //$this->assertTokenResponse($response, FALSE);
+
+  //   $verification_url = url('oauth2/tokens/' . $_REQUEST['oauth_token'], array('absolute' => TRUE));//, 'query' => array('scope' => 'gmail_extension')));
+  //   CRM_Core_Error::debug_var('$verification_url', $verification_url);
+  //   $result = self::httpRequest($verification_url);
+  //   CRM_Core_Error::debug_var('$result', $result);
+  //   $verification_response = json_decode($result->data);
+  //   CRM_Core_Error::debug_var('$verification_response', $verification_response);
+
+  //   if ($result->code == '200' && !empty($verification_response->user_id) && ($verification_response->access_token == $_REQUEST['oauth_token'])) {
+  //     // consider logged IN
+  //     $output[] = 'logged IN';
+
+  //     // DS: to programmatically force user login. It also initializes civi
+  //     //$form_state = array();
+  //     //$form_state['uid'] = $verification_response->user_id;
+  //     //CRM_Core_Error::debug_var('$form_state', $form_state);
+  //     //user_login_submit(array(), $form_state);
+  //   } else {
+  //     $output[] = 'logged OUT';
+  //   }
+
+  //   /*
+  //   $loggedIn = FALSE;
+
+  //   //check session to check the login status
+  //  /* $session =& CRM_Core_Session::singleton();
+  //   $userID = $session->get('ufID');
+
+  //   // ask user to log in to Civi, if they haven't logged in yet
+  //   if (isset($userID) || !empty($userID)) {
+  //     $loggedIn = TRUE;
+  //   }else {
+  //     $response['is_loggedin'] = $loggedIn;
+  //     $response['message'] = 'Please Login to your Civi in another tab to continue!';
+  //     CRM_Utils_JSON::output($response);
+  //     CRM_Utils_System::civiExit();
+  //   }*/
+
+  //   // test
+
+
+  //    $loggedIn = TRUE;
+
+
+  //   if (isset($_GET['email']) && !empty($_GET['email']) && $loggedIn) {
+  //     $email = $_GET['email'];
+
+  //     $response = _civicrm_api3_civi_gmailapi_civicrm_api_wrapper('Contact', 'get', array('email' => $email));
+  //   } else {
+  //     $loggedIn = FALSE;
+  //     $response['message'] = 'Some technical error. Please contact admin!';
+  //   }
+  //   $response['is_loggedin'] = $loggedIn;
+
+  //   CRM_Core_Error::debug_var('$_SERVER', $_SERVER);
+  //   // FOR GMAIL CORS request - Request header field Access-Control-Allow-Origin is not allowed by Access-Control-Allow-Headers in preflight response.
+  //   if (isset($_SERVER['HTTP_ORIGIN'])) {
+  //     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+  //     header('Access-Control-Allow-Credentials: true');
+  //     header('Access-Control-Max-Age: 86400');    // cache for 1 day
+  //   }
+  //   if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+  //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+  //       header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+  //     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+  //       header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+  //   }
+  //   CRM_Utils_JSON::output($output);
+  //   CRM_Utils_System::civiExit();
+  // }
+
+  // /**
+  //  * Performs an authorization request and returns it.
+  //  *
+  //  * Used to test authorization, the implicit flow, and the authorization_code
+  //  * grant type.
+  //  *
+  //  * @return
+  //  *   The return value of $this->httpRequest().
+  //  */
+  // static function authorizationCodeRequest($response_type, $scope = NULL) {
+  //   $clientId = 'client123id';
+  //   $query = array(
+  //     'response_type' => $response_type,//code, token
+  //     'client_id' => $clientId,
+  //     'state' => drupal_get_token($clientId),
+  //     // The "authorized" url doesn't actually exist, but we don't need it.
+  //     'redirect_uri' => url('authorized', array('absolute' => TRUE)),
+  //     // OpenID Connect requests require a nonce. Others ignore it.
+  //     'nonce' => 'test',
+  //     'scope' => 'gmail_extension',
+  //   );
+
+  //   $authorize_url = url('oauth2/authorize', array('absolute' => TRUE, 'query' => $query));
+  //   return self::httpRequest($authorize_url);
+  // }
+
+  // /**
+  //  * Performs a drupal_http_request() with additional parameters.
+  //  *
+  //  * Passes along all cookies. This ensures that the test user has access
+  //  * to the oauth2 endpoints.
+  //  *
+  //  * @param $url
+  //  *   $url: A string containing a fully qualified URI.
+  //  * @param $options
+  //  *   The options array passed along to drupal_http_request().
+  //  *
+  //  * @return
+  //  *   The result object as returned by drupal_http_request().
+  //  */
+  // static function httpRequest($url, array $options = array()) {
+  //   // Forward cookies.
+  //   $cookie_string = '';
+
+  //   //foreach ($this->cookies as $name => $data) {
+  //   //  $cookie_string .= $name . '=' . $data['value'] . ';';
+  //   //}
+
+  //   $options['headers']['Cookie'] = $cookie_string;
+
+  //   // Set other general options.
+  //   $options += array(
+  //     'max_redirects' => 0,
+  //   );
+
+  //   return drupal_http_request($url, $options);
+  // }
 }
